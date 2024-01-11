82df9e97c634252674e31545db3e20e56577b32f
Viki <hi@viki.moe>
2024-01-11 17:36:19 +0800

    chore: wip tuner

--- src/pages/tuner/index.tsx
+++ src/pages/tuner/index.tsx
@@ -1,3 +1,52 @@
+import { PitchDetector } from 'pitchy'
+import { useTranslation } from 'react-i18next'
+
+import { store } from './store'
+import { Button } from '@geist-ui/core'
+
 export function Tuner() {
-  return <div>Tuner</div>
+  const state = store.useState()
+  const { t } = useTranslation(['nav'])
+
+  async function starTuning() {
+    const audioContext = new AudioContext()
+    const analyserNode = audioContext.createAnalyser()
+
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+
+    audioContext.createMediaStreamSource(stream).connect(analyserNode)
+    const detector = PitchDetector.forFloat32Array(analyserNode.fftSize)
+    detector.minVolumeDecibels = -10
+    const input = new Float32Array(detector.inputLength)
+
+    setInterval(updatePitch, 100)
+
+    function updatePitch() {
+      console.log('updatePitch...')
+
+      analyserNode.getFloatTimeDomainData(input)
+
+      const [pitch, clarity] = detector.findPitch(input, audioContext.sampleRate)
+
+      if (!pitch) return
+
+      store.mutate.clarity = clarity
+      store.mutate.pitch = pitch
+    }
+  }
+
+  return (
+    <div>
+      <h2>{t('tuner')}</h2>
+
+      <div className='flex gap-4'>
+        <span>pitch: {state.pitch.toFixed(3)}Hz</span>
+        <span>clarity: {(state.clarity * 100).toFixed(3)}%</span>
+      </div>
+
+      <Button auto onClick={starTuning} placeholder='start'>
+        start
+      </Button>
+    </div>
+  )
 }
---
+++ src/pages/tuner/store.ts
@@ -0,0 +1,6 @@
+import { create } from '@/utils/create-store'
+
+export const store = create({
+  pitch: 0,
+  clarity: 0.2, // should be 0.9, 0.2 is for testing
+})
