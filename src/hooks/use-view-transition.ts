6af7afdc3720e80ea4f2152b758b079a264792c7
Viki <hi@viki.moe>
2024-01-12 12:12:55 +0800

    fix: fix document.startViewTransition error

--- src/hooks/use-view-transition.ts
+++ src/hooks/use-view-transition.ts
@@ -1,8 +1,13 @@
 import { AnyFunc } from '@/utils/types'
 
+// polyfill for startViewTransition
+document.startViewTransition = document.startViewTransition
+  ? document.startViewTransition
+  : (fn: AnyFunc) => fn()
+
 export const useViewTransition = () => {
   return {
-    start: (fn: AnyFunc) => document?.startViewTransition(fn) ?? fn(),
-    withTrans: (fn: AnyFunc) => () => document?.startViewTransition(fn) ?? fn(),
+    start: (fn: AnyFunc) => document.startViewTransition(fn) ?? fn(),
+    withTrans: (fn: AnyFunc) => () => document.startViewTransition(fn) ?? fn(),
   } as const
 }
