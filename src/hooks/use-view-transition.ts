faa37d4ecdda5583450e4e6c6f29d338293d5393
Viki <hi@viki.moe>
2024-01-12 11:53:01 +0800

    Update use-view-transition.ts

--- src/hooks/use-view-transition.ts
+++ src/hooks/use-view-transition.ts
@@ -2,7 +2,7 @@ import { AnyFunc } from '@/utils/types'
 
 export const useViewTransition = () => {
   return {
-    start: (fn: AnyFunc) => document.startViewTransition(fn),
-    withTrans: (fn: AnyFunc) => () => document.startViewTransition(fn),
+    start: (fn: AnyFunc) => document?.startViewTransition(fn) ?? fn(),
+    withTrans: (fn: AnyFunc) => () => document?.startViewTransition(fn) ?? fn(),
   } as const
 }
